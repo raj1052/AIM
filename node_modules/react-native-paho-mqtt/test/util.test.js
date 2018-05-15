637783f5bfc50e6c3d3747bef6feae5d980b1d7a
Nikunj Patel <rajspatel1297@gmail.com>
2018-05-15 08:52:54 +0530

    RP: changes in dir

--- node_modules/react-native-paho-mqtt/test/util.test.js
+++
@@ -1,5 +0,0 @@
-import { format } from '../src/util';
-
-test('format applies substitutions', () => {
-  expect(format({ text: 'Hi {0}, formatting is working {1}' }, ['Rob', 'OK'])).toBe('Hi Rob, formatting is working OK');
-});
